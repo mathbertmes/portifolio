49f860c9d7455b27356cb81565bdb6cad6c5a978
Matheus Bertemes <mathbertemes@gmail.com>
2024-11-29 00:35:44 -0800

    main color

--- app/components/mainContainer/mainContainer.tsx
+++ app/components/mainContainer/mainContainer.tsx
@@ -4,7 +4,7 @@ import profilePic from "@/public/profilePicGithub.jpg"
 const MainContainer = () => {
   return(
     
-      <div className=" w-full rounded-2xl flex flex-col bg-gray-700 py-10 px-5">
+      <div className=" w-full rounded-2xl flex flex-col bg-main py-10 px-5">
       <Image src={profilePic} alt="Matheus Bertemes Picture"/>
     </div>
     
--- app/components/personalContainer.tsx
+++ app/components/personalContainer.tsx
@@ -4,7 +4,7 @@ import profilePic from "@/public/profilePicGithub.jpg"
 
 const PersonalContainer = () => {
   return(
-    <div className="flex flex-col bg-gray-700 py-10 px-5 max-h-fit rounded-2xl">
+    <div className="flex flex-col bg-main py-10 px-5 max-h-fit rounded-2xl">
       <div className="flex flex-col items-center gap-5">
         <Image src={profilePic} alt="Matheus Bertemes Picture" className="rounded-xl" width={300} height={300}/>
         <h1 className="text-2xl font-bold">Matheus Bertemes</h1>
