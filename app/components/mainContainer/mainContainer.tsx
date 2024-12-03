1248e6a0ffbff6ce70975c6f2352ceb05d2492b3
Matheus Bertemes <mathbertemes@gmail.com>
2024-12-03 13:33:28 -0800

    feat: Main structure done

--- app/components/mainContainer/mainContainer.tsx
+++ app/components/mainContainer/mainContainer.tsx
@@ -1,12 +1,32 @@
-import Image from "next/image";
-import profilePic from "@/public/profilePicGithub.jpg";
+"use client"
+import { useState } from "react";
 import MenuDesktop from "./menuDesktop";
+import AboutMe from "./pages/aboutMe";
+import Resume from "./pages/resume";
+import Projects from "./pages/projects";
+import ContactMe from "./pages/contactMe";
 
 const MainContainer = () => {
+  const [page, setPage] = useState("aboutMe")
+
+  const handleChangePages = (page: string) => {
+    setPage(page)
+  }
   return (
     <div className=' w-full rounded-2xl flex flex-col bg-main py-10 px-5 relative'>
-       <MenuDesktop />
-      <Image src={profilePic} alt='Matheus Bertemes Picture' />
+       <MenuDesktop activePage={page}  handleChangePage={handleChangePages}/>
+        {page === "aboutMe" && (
+          <AboutMe />
+        )}
+        {page === "resume" && (
+          <Resume />
+        )}
+        {page === "projects" && (
+          <Projects />
+        )}
+        {page === "contactMe" && (
+          <ContactMe />
+        )}
      
     </div>
   );
