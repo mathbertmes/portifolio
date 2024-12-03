545a48eb3b91fa051f5c3528efe22bfbd0be1c85
Matheus Bertemes <mathbertemes@gmail.com>
2024-12-02 21:44:00 -0800

    main container

--- app/components/mainContainer/mainContainer.tsx
+++ app/components/mainContainer/mainContainer.tsx
@@ -4,7 +4,7 @@ import profilePic from "@/public/profilePicGithub.jpg"
 const MainContainer = () => {
   return(
     
-      <div className=" w-full rounded-2xl flex flex-col bg-main py-10 px-5">
+      <div className=" w-full rounded-2xl flex flex-col bg-main py-10 px-5 relative">
       <Image src={profilePic} alt="Matheus Bertemes Picture"/>
     </div>
     
