b31481ca2e38ce1dbbc3d58a89b24a72819bae1d
Matheus Bertemes <mathbertemes@gmail.com>
2024-12-03 13:33:13 -0800

    menu desktop

--- app/components/mainContainer/menuDesktop.tsx
+++ app/components/mainContainer/menuDesktop.tsx
@@ -1,13 +1,20 @@
 
 
-const MenuDesktop = () => {
+interface MenuDesktopType {
+  handleChangePage: (page: string) => void;
+  activePage: string;
+}
+const MenuDesktop: React.FC<MenuDesktopType> = ({
+  handleChangePage,
+  activePage
+}) => {
   return (
     <div className="absolute top-0 right-0 py-5 px-10 border-l border-b rounded-es-2xl">
       <div className="flex gap-5">
-        <p className="cursor-pointer hover:text-green-500">About me</p>
-        <p className="cursor-pointer hover:text-green-500">Resume</p>
-        <p className="cursor-pointer hover:text-green-500">Projects</p>
-        <p className="cursor-pointer hover:text-green-500">Contact me</p>
+        <p onClick={() => handleChangePage("aboutMe")} className="cursor-pointer hover:text-green-500">About me</p>
+        <p onClick={() => handleChangePage("resume")} className="cursor-pointer hover:text-green-500">Resume</p>
+        <p onClick={() => handleChangePage("projects")} className="cursor-pointer hover:text-green-500">Projects</p>
+        <p onClick={() => handleChangePage("contactMe")} className="cursor-pointer hover:text-green-500">Contact me</p>
       </div>
     </div>
   )
