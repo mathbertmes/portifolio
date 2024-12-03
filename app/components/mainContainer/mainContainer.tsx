ff62f6c68d675d4a98aecfccac1aaf8ff553f959
Matheus Bertemes <mathbertemes@gmail.com>
2024-12-03 02:39:31 -0800

    main container

--- app/components/mainContainer/mainContainer.tsx
+++ app/components/mainContainer/mainContainer.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
-import profilePic from "@/public/profilePicGithub.jpg"
+import profilePic from "@/public/profilePicGithub.jpg";
+import MenuDesktop from "./menuDesktop";
 
 const MainContainer = () => {
-  return(
-    
-      <div className=" w-full rounded-2xl flex flex-col bg-main py-10 px-5 relative">
-      <Image src={profilePic} alt="Matheus Bertemes Picture"/>
+  return (
+    <div className=' w-full rounded-2xl flex flex-col bg-main py-10 px-5 relative'>
+       <MenuDesktop />
+      <Image src={profilePic} alt='Matheus Bertemes Picture' />
+     
     </div>
-    
-  )
-}
+  );
+};
 
 export default MainContainer;
